16775655224b1c4c9e90b4523c9e3b415eca653d
nonamer <nonamer@macbook.local>
2018-12-14 19:13:20 +0800

    Initial Commit

---
+++ LearningLogTests/LearningLogTests.swift
@@ -0,0 +1,34 @@
+//
+//  LearningLogTests.swift
+//  LearningLogTests
+//
+//  Created by nonamer on 2018/12/14.
+//  Copyright © 2018年 龙洪杰. All rights reserved.
+//
+
+import XCTest
+@testable import LearningLog
+
+class LearningLogTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
